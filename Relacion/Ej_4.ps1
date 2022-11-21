7173f80dd9080eb4437a44e37c699d3fe3c1913c
Guillermo Montero <113906820+drain113@users.noreply.github.com>
2022-11-22 00:08:15 +0100

    Delete Relacion directory

--- Relacion/Ej_4.ps1
+++
@@ -1,6 +0,0 @@
-# Dados tres números como argumentos, obtener la suma y mostrarla en pantalla junto con
-# un mensaje que lo aclare: “La suma es .....”.
-
-$suma= $args[0] + $args[1] + $args[2]
-
-Write-Output "La suma es: " $suma
